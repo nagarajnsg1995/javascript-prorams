1a0f1e7558bdb7b86a62e355935e2ecdc091f983
nagarajnsg1995 <nagarajasbg023@gmail.com>
2022-07-31 19:15:24 +0530

    Added temperature conversion program

---
+++ functional-programs/temprature.js
@@ -0,0 +1,21 @@
+var prompt = require('prompt-sync')();
+
+var C = prompt("Enter celsius between 0°C and 100°C : ");
+var F = prompt("Enter celsius between 32°F and 212°F : ");
+
+
+
+
+let randomCheck =( Math.floor(Math.random() * 10) % 2);
+
+switch(randomCheck) {
+    case 0: 
+    let degF = (C * 9/5) + 32;
+        console.log("celsius to fahreneheit is : " + degF);
+        break;
+    case 1:
+        let degC = (F - 32) * 5/9;
+        console.log("fahreneheit to celsius is : " + degC);
+        break;
+        
+}
